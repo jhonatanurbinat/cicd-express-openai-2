c4fb6770ea52aa2a3d2b3b9095ccf510c9b49a37
Jhonatan Urbina <jhonatanurbinat@gmail.com>
2025-08-31 17:54:43 -0500

    commit

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('API Endpoints', () => {
-	test("GET / responde '¡Hola Mundo!'", async () => {
-		const res = await request(app).get('/');
-		expect(res.statusCode).toBe(200);
-		expect(res.text).toBe('¡Hola Mundo!');
-	});
+  test('GET / responde \'¡Hola Mundo!\'', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('¡Hola Mundo!');
+  });
 });
