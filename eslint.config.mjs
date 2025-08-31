c93d19a76100fca35ba2b3dbd81524dddf2eeba2
Jhonatan Urbina <jhonatanurbinat@gmail.com>
2025-08-31 13:48:19 -0500

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+// Change the import from "eslint/config" to "eslint"
+import { defineConfig } from "eslint";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
